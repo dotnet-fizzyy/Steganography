ecdd1cf8b8da7130e641e3c2c0b8d9854e96cadb
Artem Radchikov <art.radchikov@mail.ru>
2020-05-27 02:25:52 +0300

    add cyclic cod method

--- Программное средство/stegano/KelErnBor/Algorithm/Aditional Coding/CyclicCod.cs
+++ Программное средство/stegano/KelErnBor/Algorithm/Aditional Coding/CyclicCod.cs
@@ -10,23 +10,62 @@ namespace Stegano.Algorithm.Aditional_Coding
 {
     public class CyclicCod : ICod
     {
-        public string Coding(string input)
+        Matrix matrix;
+        public CyclicCod(int k, string generatPolinom)
         {
-            ShowMetroMessageBox("CyclicCod", "CyclicCod");
-            return "";
+            matrix = new Matrix(k, generatPolinom);
         }
 
-        public string DeCoding(string input)
+        public string Coding(string message)
         {
-            ShowMetroMessageBox("CyclicCod", "CyclicCod");
-            return "";
+            var result = new StringBuilder();
+            int i = 0;
+            int wordsCount = (int)Math.Ceiling((decimal)(message.Length / matrix.K))+1;
+            int newLength = wordsCount * matrix.K;
+
+            message = message.PadRight(newLength, '0');
+
+            while (i < wordsCount)
+            {
+                ShowMetroMessageBox(Cod(message.Substring(matrix.K * i, matrix.K)),i.ToString());
+                result.Append(Cod(message.Substring(matrix.K * i++, matrix.K)));
+            }
+
+            return Matrix.ReadByCols(result.ToString(), matrix.N, wordsCount);
         }
 
-        public override string ToString()
+        public string DeCoding(string message)
         {
-            return "Кодирование при помощи Циклического кода";
+            var result = new StringBuilder();
+            int i = 0;
+            int wordsCount = (int)Math.Ceiling((decimal)(message.Length / matrix.N));
+            int newLength = wordsCount * matrix.N;
+            message = message.PadRight(newLength, '0');
+            message = Matrix.ReadByCols(message, wordsCount, matrix.N);
+
+            while (i < wordsCount)
+            {
+                result.Append(DeCod(message.Substring(matrix.N * i++, matrix.N)));
+            }
+            int Count = result.Length - (result.Length % 16);
+
+            return result.ToString().Substring(0, Count);
         }
 
+        public string Cod(string messageStrBin)
+        {
+            matrix.InfWord = messageStrBin;
+
+            return matrix.CodWord;
+        }
+
+        public string DeCod(string messageStrBin)
+        {
+            return matrix.CorrectValue(messageStrBin, matrix.FindeSindrom(messageStrBin)).Substring(0,matrix.K);
+        }
+
+
+        public override string ToString() => "Кодирование при помощи циклического кода";
         private void ShowMetroMessageBox(string title, string message)
         {
             var dialog = new ModernDialog()
