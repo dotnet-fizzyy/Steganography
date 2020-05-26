a9aedf3e484297ebd456c0489d5446108d54e682
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2020-05-26 21:24:11 +0300

    Update VO method with our's

--- Программное средство/stegano/KelErnBor/ViewModel/Color/HideColorViewModel.cs
+++ Программное средство/stegano/KelErnBor/ViewModel/Color/HideColorViewModel.cs
@@ -12,126 +12,126 @@ using Stegano.Model;
 
 namespace Stegano.ViewModel.ColorSteg
 {
-    public class HideColorViewModel:ViewModelBase
+    public class HideColorViewModel:BaseHideViewModel
     {
         #region Properties
 
-        private string fullPathToOrigFile;
-        public string FullPathToOrigFile
-        {
-            get { return "Путь к файлу: " + fullPathToOrigFile; }
-            set { fullPathToOrigFile = value; RaisePropertyChanged(); }
-        }
+        //private string fullPathToOrigFile;
+        //public string FullPathToOrigFile
+        //{
+        //    get { return "Путь к файлу: " + fullPathToOrigFile; }
+        //    set { fullPathToOrigFile = value; RaisePropertyChanged(); }
+        //}
 
-        private string countLettersIsCanHide;
-        public string CountLettersIsCanHide
-        {
-            get { return $"Количество символов, которые можно скрыть: {countLettersIsCanHide}"; }
-            set
-            {
-                countLettersIsCanHide = value;
-                RaisePropertyChanged();
+        //private string countLettersIsCanHide;
+        //public string CountLettersIsCanHide
+        //{
+        //    get { return $"Количество символов, которые можно скрыть: {countLettersIsCanHide}"; }
+        //    set
+        //    {
+        //        countLettersIsCanHide = value;
+        //        RaisePropertyChanged();
 
-            }
-        }
+        //    }
+        //}
 
-        private string textForHide = "";
-        public string TextForHide
-        {
-            get { return textForHide; }
-            set
-            {
-                TextForHideChanged(value);
-                RaisePropertyChanged();
-            }
-        }
+        //private string textForHide = "";
+        //public string TextForHide
+        //{
+        //    get { return textForHide; }
+        //    set
+        //    {
+        //        TextForHideChanged(value);
+        //        RaisePropertyChanged();
+        //    }
+        //}
 
 
-        private string countLettersForHide;
-        public string CountLettersForHide
-        {
-            get { return $"Введено символов: {countLettersForHide}"; }
-            set
-            {
-                countLettersForHide = value;
-                RaisePropertyChanged();
-            }
-        }
+        //private string countLettersForHide;
+        //public string CountLettersForHide
+        //{
+        //    get { return $"Введено символов: {countLettersForHide}"; }
+        //    set
+        //    {
+        //        countLettersForHide = value;
+        //        RaisePropertyChanged();
+        //    }
+        //}
 
-        private string keyStatus;
-        public string KeyStatus
-        {
-            private get { return keyStatus; }
-            set
-            {
-                keyStatus = value;
-                RaisePropertyChanged();
-            }
-        }
+        //private string keyStatus;
+        //public string KeyStatus
+        //{
+        //    private get { return keyStatus; }
+        //    set
+        //    {
+        //        keyStatus = value;
+        //        RaisePropertyChanged();
+        //    }
+        //}
 
-        private SolidColorBrush keyStatusColor;
-        public SolidColorBrush KeyStatusColor
-        {
-            get { return keyStatusColor; }
-            set
-            {
-                keyStatusColor = value;
-                RaisePropertyChanged();
-            }
-        }
+        //private SolidColorBrush keyStatusColor;
+        //public SolidColorBrush KeyStatusColor
+        //{
+        //    get { return keyStatusColor; }
+        //    set
+        //    {
+        //        keyStatusColor = value;
+        //        RaisePropertyChanged();
+        //    }
+        //}
 
-        private bool isTextForHideEnabled;
-        public bool IsTextForHideEnabled
-        {
-            get { return isTextForHideEnabled; }
-            set
-            {
-                isTextForHideEnabled = value;
-                RaisePropertyChanged();
-            }
-        }
+        //private bool isTextForHideEnabled;
+        //public bool IsTextForHideEnabled
+        //{
+        //    get { return isTextForHideEnabled; }
+        //    set
+        //    {
+        //        isTextForHideEnabled = value;
+        //        RaisePropertyChanged();
+        //    }
+        //}
 
         private string sourceString = string.Empty;
 
-        public CheckBoxModel RandomCheckBox { get; set; }
+        //public CheckBoxModel RandomCheckBox { get; set; }
 
-        public CheckBoxModel RSACheckBox { get; set; }
+        //public CheckBoxModel RSACheckBox { get; set; }
 
-        public CheckBoxModel AdditionalBitsCheckBox { get; set; }
+        //public CheckBoxModel AdditionalBitsCheckBox { get; set; }
 
-        public CheckBoxModel VisibleColorCheckBox { get; set; }
+        //public CheckBoxModel VisibleColorCheckBox { get; set; }
 
-        public CheckBoxModel SmartHidingCheckBox { get; set; }
+        //public CheckBoxModel SmartHidingCheckBox { get; set; }
 
 
-        private bool isHideInformationButtonEnabled;
-        public bool IsHideInformationButtonEnabled
-        {
-            get { return isHideInformationButtonEnabled; }
-            set
-            {
-                isHideInformationButtonEnabled = value;
-                RaisePropertyChanged();
-            }
-        }
+        //private bool isHideInformationButtonEnabled;
+        //public bool IsHideInformationButtonEnabled
+        //{
+        //    get { return isHideInformationButtonEnabled; }
+        //    set
+        //    {
+        //        isHideInformationButtonEnabled = value;
+        //        RaisePropertyChanged();
+        //    }
+        //}
 
         #endregion
 
         #region RelayCommands
 
-        public RelayCommand OpenDocumentRelayCommand { get; private set; }
-        public RelayCommand HideInformationRelayCommand { get; private set; }
+        //public RelayCommand OpenDocumentRelayCommand { get; private set; }
+        //public RelayCommand HideInformationRelayCommand { get; private set; }
 
         #endregion
 
         #region VARS
 
-        private OpenFileDialog openFileDialog;
+        //private OpenFileDialog openFileDialog;
 
-        private string pathToDirOrigFile;
-        private string filenameOrigFile;
+        //private string pathToDirOrigFile;
+        //private string filenameOrigFile;
 
-        private int maxLettersIsCanHide;
+        //private int maxLettersIsCanHide;
         #endregion
 
         #region Constructor and Initializers
@@ -147,7 +147,7 @@ namespace Stegano.ViewModel.ColorSteg
 
         private void RelayInit()
         {
-            OpenDocumentRelayCommand = new RelayCommand(OpenDocument);
+            //OpenDocumentRelayCommand = new RelayCommand(OpenDocument);
             HideInformationRelayCommand = new RelayCommand(HideInformation);
         }
 
@@ -165,43 +165,43 @@ namespace Stegano.ViewModel.ColorSteg
             IsTextForHideEnabled = false;
             IsHideInformationButtonEnabled = false;
 
-            RSACheckBox = new CheckBoxModel();
-            VisibleColorCheckBox = new CheckBoxModel();
-            AdditionalBitsCheckBox = new CheckBoxModel();
-            RandomCheckBox = new CheckBoxModel();
-            SmartHidingCheckBox = new CheckBoxModel();
+            //RSACheckBox = new CheckBoxModel();
+            //VisibleColorCheckBox = new CheckBoxModel();
+            //AdditionalBitsCheckBox = new CheckBoxModel();
+            //RandomCheckBox = new CheckBoxModel();
+            //SmartHidingCheckBox = new CheckBoxModel();
         }
 
         #endregion
 
         #region RelayMethods
-        private void OpenDocument()
-        {
-            if (OpenFileDialog(openFileDialog) != null)
-            {
-                FullPathToOrigFile = openFileDialog.FileName;
-                filenameOrigFile = openFileDialog.SafeFileName;
-                pathToDirOrigFile = fullPathToOrigFile.Substring(0, fullPathToOrigFile.Length - filenameOrigFile.Length);
+        //private void OpenDocument()
+        //{
+        //    if (OpenFileDialog(openFileDialog) != null)
+        //    {
+        //        FullPathToOrigFile = openFileDialog.FileName;
+        //        filenameOrigFile = openFileDialog.SafeFileName;
+        //        pathToDirOrigFile = fullPathToOrigFile.Substring(0, fullPathToOrigFile.Length - filenameOrigFile.Length);
 
-                CountLettersIsCanHide = HideColorModel.HowMuchLettersICanHide(fullPathToOrigFile).ToString();
-                maxLettersIsCanHide = Int32.Parse(countLettersIsCanHide);
-                if (Int32.Parse(countLettersIsCanHide) > 0)
-                {
-                    IsTextForHideEnabled = true;
-                    IsHideInformationButtonEnabled = true;
+        //        CountLettersIsCanHide = HideColorModel.HowMuchLettersICanHide(fullPathToOrigFile).ToString();
+        //        maxLettersIsCanHide = Int32.Parse(countLettersIsCanHide);
+        //        if (Int32.Parse(countLettersIsCanHide) > 0)
+        //        {
+        //            IsTextForHideEnabled = true;
+        //            IsHideInformationButtonEnabled = true;
 
-                    RandomCheckBox.IsEnabled = true;
-                    RSACheckBox.IsEnabled = true;
-                    AdditionalBitsCheckBox.IsEnabled = true;
-                    VisibleColorCheckBox.IsEnabled = true;
-                    SmartHidingCheckBox.IsEnabled = true;
-                }
-                else
-                {
-                    ShowMetroMessageBox("Ошибка", "В данном файле невозможно скрыть информацию.");
-                }
-            }
-        }
+        //            RandomCheckBox.IsEnabled = true;
+        //            RSACheckBox.IsEnabled = true;
+        //            AdditionalBitsCheckBox.IsEnabled = true;
+        //            VisibleColorCheckBox.IsEnabled = true;
+        //            SmartHidingCheckBox.IsEnabled = true;
+        //        }
+        //        else
+        //        {
+        //            ShowMetroMessageBox("Ошибка", "В данном файле невозможно скрыть информацию.");
+        //        }
+        //    }
+        //}
 
         private async void HideInformation()
         {       
@@ -218,28 +218,42 @@ namespace Stegano.ViewModel.ColorSteg
                 string pathToNewFile = DocumentHelper.CopyFile(pathToDirOrigFile, filenameOrigFile);
                 bool isSuccesful = false;
 
-                textForHide = (RSACheckBox.IsChecked)
-                    ? Converter.RsaCryptor(TextForHide, pathToDirOrigFile)
-                    : Converter.StringToBinary(TextForHide);
+                //textForHide = (RSACheckBox.IsChecked)
+                //    ? Converter.RsaCryptor(TextForHide, pathToDirOrigFile)
+                //    : Converter.StringToBinary(TextForHide);
 
-                textForHide = (AdditionalBitsCheckBox.IsChecked)
-                    ? HideColorModel.AddAdditionalBits(textForHide)
-                    : textForHide;
+                //textForHide = (AdditionalBitsCheckBox.IsChecked)
+                //    ? HideColorModel.AddAdditionalBits(textForHide)
+                //    : textForHide;
+                textForHide = SelectedCryptMethod?.Encrypt(textForHide, pathToDirOrigFile) ?? textForHide;
 
-                    HideColorModel codeModel = new HideColorModel(pathToNewFile);
+                textForHide = SelectedCodMethod?.Coding(SelectedCryptMethod != null ? textForHide : Converter.StringToBinary(TextForHide)) ?? TextForHide;
+
+                if (SelectedCryptMethod == null && SelectedCodMethod == null)
+                {
+                    textForHide = Converter.StringToBinary(textForHide);
+                }
+
+                HideColorModel codeModel = new HideColorModel(pathToNewFile);
                     isSuccesful = await codeModel.HideInformation(textForHide.ToCharArray(), RandomCheckBox.IsChecked, VisibleColorCheckBox.IsChecked, SmartHidingCheckBox.IsChecked);
+
+                var hash = SelectedHashMethod?.GetHash(SelectedCryptMethod == null || SelectedCodMethod != null ? TextForHide : Converter.BinaryToString(TextForHide)) ?? TextForHide;
+                if (!string.IsNullOrWhiteSpace(hash))
+                {
+                    MD5.SaveHash(pathToDirOrigFile, hash); //Mocked until base class will not be implemented
+                }
 
                 if (isSuccesful)
                 {
                     ShowMetroMessageBox("Информация", "Скрытие информации прошло успешно.\n\nПуть к измененному файлу: " + pathToNewFile);
-                    KeyStatus = (RSACheckBox.IsChecked == true) ? "сгенерирован" : "выключен RSA";
-                    KeyStatusColor = (RSACheckBox.IsChecked == true) ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Black);
+                    //KeyStatus = (RSACheckBox.IsChecked == true) ? "сгенерирован" : "выключен RSA";
+                    //KeyStatusColor = (RSACheckBox.IsChecked == true) ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Black);
                 }
                 else
                 {
                     ShowMetroMessageBox("Информация", "Во время выполнения произошла ошибка.");
-                    KeyStatus = (RSACheckBox.IsChecked == true) ? "ошибка генерации" : "выключен RSA";
-                    KeyStatusColor = (RSACheckBox.IsChecked == true) ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Black);
+                    //KeyStatus = (RSACheckBox.IsChecked == true) ? "ошибка генерации" : "выключен RSA";
+                    //KeyStatusColor = (RSACheckBox.IsChecked == true) ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Black);
                 }
             }
             else
@@ -253,48 +267,48 @@ namespace Stegano.ViewModel.ColorSteg
         #endregion
 
 
-        private void TextForHideChanged(string text)
-        {
-            textForHide = text;
-            CountLettersForHide = textForHide.Length.ToString();
-            int letterWeigth = 0;
-            if (AdditionalBitsCheckBox != null)
-                letterWeigth = AdditionalBitsCheckBox.IsChecked ? Int32.Parse(countLettersForHide) * 4 : Int32.Parse(countLettersForHide);
-            CountLettersIsCanHide = (maxLettersIsCanHide - letterWeigth).ToString();
-        }
+    //    private void TextForHideChanged(string text)
+    //    {
+    //        textForHide = text;
+    //        CountLettersForHide = textForHide.Length.ToString();
+    //        int letterWeigth = 0;
+    //        if (AdditionalBitsCheckBox != null)
+    //            letterWeigth = AdditionalBitsCheckBox.IsChecked ? Int32.Parse(countLettersForHide) * 4 : Int32.Parse(countLettersForHide);
+    //        CountLettersIsCanHide = (maxLettersIsCanHide - letterWeigth).ToString();
+    //    }
 
-        private OpenFileDialog OpenFileDialog(OpenFileDialog openFileDialog)
-        {
-            try
-            {
-                openFileDialog.Filter = "Файлы Microsoft Word|*.doc;*.docx; | Все файлы|*.*";
-                openFileDialog.FilterIndex = 1;
-                openFileDialog.RestoreDirectory = true;
+    //    private OpenFileDialog OpenFileDialog(OpenFileDialog openFileDialog)
+    //    {
+    //        try
+    //        {
+    //            openFileDialog.Filter = "Файлы Microsoft Word|*.doc;*.docx; | Все файлы|*.*";
+    //            openFileDialog.FilterIndex = 1;
+    //            openFileDialog.RestoreDirectory = true;
 
-                if (openFileDialog.ShowDialog() == true)
-                {
-                    return openFileDialog;
-                }
+    //            if (openFileDialog.ShowDialog() == true)
+    //            {
+    //                return openFileDialog;
+    //            }
 
-                return null;
-            }
-            catch (Exception exception)
-            {
-                ShowMetroMessageBox("Error", exception.Message);
-            }
+    //            return null;
+    //        }
+    //        catch (Exception exception)
+    //        {
+    //            ShowMetroMessageBox("Error", exception.Message);
+    //        }
 
-            return null;
-        }
+    //        return null;
+    //    }
 
-        private void ShowMetroMessageBox(string title, string message)
-        {
-            var dialog = new ModernDialog()
-            {
-                Title = title,
-                Content = message                
-            };
+    //    private void ShowMetroMessageBox(string title, string message)
+    //    {
+    //        var dialog = new ModernDialog()
+    //        {
+    //            Title = title,
+    //            Content = message                
+    //        };
 
-            dialog.ShowDialog();
-        } 
+    //        dialog.ShowDialog();
+    //    } 
     }
 }
