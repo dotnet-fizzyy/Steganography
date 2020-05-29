0decaa482c947a3795e8ce212e36254ef830ff59
Artem Radchikov <art.radchikov@mail.ru>
2020-05-30 00:09:07 +0300

    Debug font hide and show methods

--- Программное средство/stegano/KelErnBor/ViewModel/Font/HideFontViewModel.cs
+++ Программное средство/stegano/KelErnBor/ViewModel/Font/HideFontViewModel.cs
@@ -27,6 +27,8 @@ namespace Stegano.ViewModel.Font
         public string OneFontName { get; set; }
         public string ZeroFontName { get; set; }
 
+        private string sourceString = string.Empty;
+
         #endregion
 
         #region Constructor and Initializers
@@ -41,30 +43,45 @@ namespace Stegano.ViewModel.Font
 
         #region RelayMethods
        
+
         private async void HideInformation()
         {
             if (textForHide.Length > 0)
             {
+                sourceString = textForHide;
+                TimeForCrypting = string.Empty;
+
                 string pathToNewFile = DocumentHelper.CopyFile(pathToDirOrigFile, filenameOrigFile);
                 bool isSuccesful = false;
-                
-                //textForHide = (RSACheckBox.IsChecked)
-                //    ? Converter.RsaCryptor(TextForHide, pathToDirOrigFile)
-                //    : Converter.StringToBinary(TextForHide);
 
-                //textForHide = SelectedCodMethod?.Coding(Converter.StringToBinary(TextForHide)) ?? Converter.StringToBinary(TextForHide);
-                textForHide = messageTransformation(Converter.StringToBinary(TextForHide), out string hash);
+                Stopwatch.Start();
+                sourceString = SelectedCryptMethod?.Encrypt(sourceString, pathToDirOrigFile) ?? sourceString;
+
+                sourceString = SelectedCodMethod?.Coding(SelectedCryptMethod != null ? sourceString : Converter.StringToBinary(sourceString)) ?? sourceString;
 
                 HideFontModel codeModel = new HideFontModel(pathToNewFile);
-                isSuccesful = await codeModel.HideInformation(textForHide.ToCharArray(), CurrentShift, RandomCheckBox.IsChecked, VisibleColorCheckBox.IsChecked, OneFontName, ZeroFontName);
+                isSuccesful = await codeModel.HideInformation(sourceString.ToCharArray(), CurrentShift, RandomCheckBox.IsChecked, VisibleColorCheckBox.IsChecked, OneFontName, ZeroFontName);
+
+                var hash = SelectedHashMethod?.GetHash(SelectedCryptMethod == null || SelectedCodMethod != null ? sourceString : Converter.BinaryToString(sourceString));
+                if (!string.IsNullOrWhiteSpace(hash))
+                {
+                    MD5.SaveHash(pathToDirOrigFile, hash); //Mocked until base class will not be implemented
+                }
+
+                Stopwatch.Stop();
+                TimeForCrypting = Math.Round(Stopwatch.Elapsed.TotalSeconds, 2).ToString() + " сек.";
 
                 if (isSuccesful)
                 {
                     ShowMetroMessageBox("Информация", "Скрытие информации прошло успешно.\n\nПуть к измененному файлу: " + pathToNewFile);
+                    KeyStatus = (SelectedCryptMethod != null) ? "сгенерирован" : "выключен";
+                    KeyStatusColor = (SelectedCryptMethod != null) ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Black);
                 }
                 else
                 {
                     ShowMetroMessageBox("Информация", "Во время выполнения произошла ошибка.");
+                    KeyStatus = (SelectedCryptMethod != null) ? "ошибка генерации" : "выключен RSA";
+                    KeyStatusColor = (SelectedCryptMethod != null) ? new SolidColorBrush(Colors.Red) : new SolidColorBrush(Colors.Black);
                 }
             }
             else
@@ -73,7 +90,6 @@ namespace Stegano.ViewModel.Font
             }
             //TextForHide = String.Empty;
         }
-
 
         #endregion
     }
